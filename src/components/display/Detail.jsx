d42eb17633978e66dc5de49ba7670bef988aa20e
Patrick Wilson <pmwilson333@gmail.com>
2020-10-26 23:16:45 -0700

    updated http request to https

--- src/components/display/Detail.jsx
+++ src/components/display/Detail.jsx
@@ -5,7 +5,7 @@ const Detail = ({ details }) => {
   return (
     <div>Detail Screen
       <h3>{details.name}</h3>
-      <img src={details.photoUrl}/>
+      <img src={details.photoUrl} />
       <p>Eye Color: {details.eye}</p>
       <p>Hair Color: {details.hair}</p>
       {details.weapon && <p>Ability: {details.weapon}</p>}
