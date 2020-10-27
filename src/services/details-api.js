3f7015eef08e4fac79a6f46501d93d1cc3fc41d4
Patrick Wilson <pmwilson333@gmail.com>
2020-10-26 23:22:13 -0700

    added cors anywhere

--- src/services/details-api.js
+++ src/services/details-api.js
@@ -1,6 +1,6 @@
 
 export async function getDetails(id) {
-  const res = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters/${id}`);
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/https://last-airbender-api.herokuapp.com/api/v1/characters/${id}`);
   const details = await res.json();
 
   return details;
